bf59b5b9a8e19cdc2f9e8ca7c5f293fe2e6489a4
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2024-01-15 14:20:26 +0800

    use env vars

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTkzNjJkMjM0MTUyYzc3YTg0YTUwZGEiLCJpYXQiOjE3MDQxNTc5MDcsImV4cCI6MTcwNTk1NzkwNywidHlwZSI6ImFjY2VzcyJ9.zA0Zw1bsyZMQhDnI_nPEARyZ25i8yHwbKT5bOs8OVVE";
-export const API_URL = "http://localhost:3000/v1"
+export const API_URL = import.meta.env.VITE_APP_API_URL ? `${import.meta.env.VITE_APP_API_URL}/v1` : "http://localhost:3000/v1"
